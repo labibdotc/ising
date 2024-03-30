5dcddd21703d0f922466676c2759b04199df08d0
twalla01 <trevor.wallace@tufts.edu>
2024-03-30 10:56:49 -0400

    Added energy plotting functions to main

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ param_N = 10
 param_T = 0.000001
 param_kB = 1#1.380649E-23
 param_J = 1
-param_ncycles = 150
+param_ncycles = 500
 
-#
 # Create the lattice using the parameters
-#
 globalLattice = matrix.MatrixLattice(param_J, param_N)
 
 # Initialize arrays for plotting
 magnetizationArr = []
+energiesArr = []
 tempArr = []
 
 #
@@ -35,16 +34,25 @@ tempArr = []
 #   param_T = param_T + 0.01
 
 ## Wolff ##
-for n in range(500):
+for n in range(50):
   energies_w, mag_w, _ = algorithms.runWolff(globalLattice, plt, param_ncycles, param_T, param_J)
   mag_w = np.absolute(mag_w[100:])
+  energies_w = np.absolute(energies_w[100:])
   print(np.std(mag_w))
+  print(np.std(energies_w))
   magnetizationArr.append(np.std(mag_w))
+  energiesArr.append(np.std(energies_w))
   tempArr.append(param_T)
-  param_T = param_T + 0.01
+  param_T = param_T + 0.1
 
 plt.plot(tempArr, magnetizationArr)
 plt.xlabel('Temperature (Nondimensionalized)')
 plt.ylabel('Average Abs Val Magnetization (Stable Zone)')
 plt.title('Magnetization vs. Temperature')
+plt.show()
+
+plt.plot(tempArr, energiesArr)
+plt.xlabel('Temperature (Nondimensionalized)')
+plt.ylabel('Average Abs Val Energy (Stable Zone)')
+plt.title('Energy vs. Temperature')
 plt.show()
